6305a0123606d8dec0f42bf7803aafdb8150fa64
ikaiguang <ikaiguang@163.com>
2024-12-04 00:16:57 +0800

    feat: makefile

--- api/makefile_protoc.mk
+++ api/makefile_protoc.mk
@@ -3,20 +3,16 @@ override ABSOLUTE_PATH := $(patsubst %/,%,$(dir $(ABSOLUTE_MAKEFILE)))
 override REL_PROJECT_PATH := $(subst $(PROJECT_ABS_PATH)/,,$(ABSOLUTE_PATH))
 
 # saas services
-SAAS_SERVICE_API_FILES := $(shell find ./$(REL_PROJECT_PATH) -name "*.proto")
-#SAAS_SERVICE_INTERNAL_PROTO := "app/nodeid-service/internal/conf/config.conf.proto"
-SAAS_SERVICE_INTERNAL_PROTO := ""
-SAAS_SERVICE_PROTO_FILES := ""
-ifneq ($(SAAS_SERVICE_INTERNAL_PROTO), "")
-	SAAS_SERVICE_PROTO_FILES=$(SAAS_SERVICE_API_FILES) $(SAAS_SERVICE_INTERNAL_PROTO)
-else
-	SAAS_SERVICE_PROTO_FILES=$(SAAS_SERVICE_API_FILES)
-endif
 .PHONY: protoc-api-protobuf
 # protoc :-->: generate services api protobuf
 protoc-api-protobuf:
 	@echo "# generate services api protobuf"
-	$(call protoc_protobuf,$(SAAS_SERVICE_PROTO_FILES))
+	$(MAKE) protoc-nodeid-protobuf
+	$(MAKE) protoc-nodeid-v1-protobuf
+	$(MAKE) protoc-ping-protobuf
+	$(MAKE) protoc-ping-v1-protobuf
+	$(MAKE) protoc-testdata-protobuf
+	$(MAKE) protoc-testdata-v1-protobuf
 
 # specified server
 SAAS_SERVICE_SPECIFIED_FILES := $(shell find ./$(REL_PROJECT_PATH)/${service} -name "*.proto")
