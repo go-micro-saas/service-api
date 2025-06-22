b70ad6c334b66f408ed99008dcea553ef7544971
chenkaiguang <ckaiguang@163.com>
2025-06-23 01:31:53 +0800

    feat: account service

--- app/account-service/account.api_test.go
+++ app/account-service/account.api_test.go
@@ -11,13 +11,27 @@ import (
 func TestHTTPClient_Xxx(t *testing.T) {
 	var (
 		ctx = context.Background()
-		req = &accountresourcev1.PingReq{}
+		req = &accountresourcev1.PingReq{
+			Message: "hello world",
+		}
 	)
 	resp, err := accountV1HTTP.Ping(ctx, req)
 	require.Nil(t, err)
-	t.Logf("==> resp: %#v\n", resp)
+	t.Logf("==> TestHTTPClient_Xxx resp: %#v\n", resp)
+	t.Logf("==> TestHTTPClient_Xxx resp.Data: %#v\n", resp.GetData())
 }
 
+// go test -v -count 1 ./app/account-service -run TestGRPCClient_Xxx
 func TestGRPCClient_Xxx(t *testing.T) {
+	var (
+		ctx = context.Background()
+		req = &accountresourcev1.PingReq{
+			Message: "hello world",
+		}
+	)
+	resp, err := accountV1GRPC.Ping(ctx, req)
+	require.Nil(t, err)
+	t.Logf("==> TestGRPCClient_Xxx resp: %#v\n", resp)
+	t.Logf("==> TestGRPCClient_Xxx resp.Data: %#v\n", resp.GetData())
 
 }
